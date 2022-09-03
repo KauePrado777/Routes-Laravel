83485092117acac0451da445e63abcd9bcdc02b6
KauePrado777 <kauecarlos218@gmail.com>
2022-09-02 21:53:15 -0300

    first commit

---
+++ resources/views/Proposito.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="/css/style.css" rel="stylesheet">
+
+
+</head>
+<body class="body2">
+   
+<header class="header1">
+        <div id="title">
+            <h1>Warden</h1>
+            
+        </div>
+
+        <ul>
+            <a href="#"><li>Início</li></a>
+            <a href="#"><li>Serviços</li></a>
+            <a href="#"><li>Sobre</li></a>
+            <a href="#"><li>Contato</li></a>
+            <a href="#" id="inscreva-se-btn"><li>Já tem uma conta?</li></a>
+        </ul>
+    </header>
+
+    <main class="main2">
+        <aside>
+            <h2><span class="span2">Propósito</span></h2>
+            <h2>do aplicativo</h2>
+            <p class="p2">
+                O projeto Warden tem como objetivo ajudar os moradores a "preverem" algum alagamento, para assim se previnir e tomar medidas necessárias.
+            </p>
+            <form>
+                <input type="text" placeholder="Nome">
+                <input type="email" placeholder="E-mail">
+               
+                <a href= "#"><input type="submit" value="Enviar "></a>
+                
+                
+
+            </form>
+        </aside>
+
+        <article>
+            <img src="/images/logobranca.png" alt="logo">
+        </article>
+    </main>
+    
+        
+
+</body>
+</html>
---
+++ resources/views/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="pt"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="startup">
+    <meta name="description" content="">
+    <title>index</title>
+
+    
+    <link rel="stylesheet" href="css/style.css" media="screen">
+    <link rel="stylesheet" href="css/styles.css" media="screen">
+    <link rel="stylesheet" href="css/style1.css" media="screen">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    <meta name="theme-color" content="#478ac9">
+  </head>
+  <body data-home-page="Página-Inicial.html" data-home-page-title="Página Inicial" class="u-body u-xl-mode" data-lang="pt">
+    <section class="u-align-left u-clearfix u-palette-1-base u-section-1" id="carousel_269d">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-custom-font u-font-montserrat u-text u-text-body-alt-color u-text-1">Warden<br>
+          <span style="font-size: 13.75rem;">App</span>
+        </h1>
+
+        <img class="u-image u-image-default u-image-1" src="images/5645.png" alt="" data-image-width="200px" data-image-height="200px">
+        <p class="u-align-left u-text u-text-body-alt-color u-text-2"> Aplicativo de alerta de enchente, para as areas de riscos da baixada Santista.</p>
+        <a href="proposito" class="u-active-palette-1-light-2 u-border-none u-btn u-btn-round u-button-style u-hover-palette-1-light-2 u-radius-50 u-text-active-white u-text-body-color u-text-hover-white u-white u-btn-2">Propósito</a>
+      </div>
+    </section>
+  
+</body></html>
